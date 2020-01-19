e355441c7368ec0ee41300b8a35dbd196a09c1bc
key999 <key999@protonmail.com>
2020-01-19 16:40:00 +0100

    update #1

--- main.py
+++ main.py
@@ -2,27 +2,25 @@
 
 from os import popen, system
 
-
-class Foo:
-    def __init__(self):
-        self.PATTERNS = {'failed': 0,
-                         'incorrect': 0,
-                         'root': 0}
-        self.LOG_FILE = 'logs'
-        self.FLAG_FILE = './flag'
-        self.TAIL_LINES = 100
-        self.LAST_LOG = None
-
-    def find_log(self):
-        form = "tail -n {0} {1} | grep -i '{2}'".format(self.TAIL_LINES, self.LOG_FILE, self.PATTERNS['failed'])
-        a = popen(form, 'r').read()
-
-        return 0 if a == '' else 1
-
-    @staticmethod
-    def bruteforce_detected(self):
-        system("echo 1 > {0}".format(self.FLAG_FILE))
+PATTERNS = {'failed': 0,
+            'incorrect': 0,
+            'root': 0}
+LOG_FILE = 'logs'
+FLAG_FILE = './flag'
+TAIL_LINES = 100
+LAST_LOG = None
 
 
-x = Foo()
-x.find_log()
+def find_log():
+    form = "tail -n {0} {1} | grep -i '{2}'".format(TAIL_LINES, LOG_FILE, PATTERNS['failed'])
+    a = popen(form, 'r').read()
+    print(a)
+
+    return 0 if a == '' else 1
+
+
+def bruteforce_detected():
+    system("echo 1 > {0}".format(FLAG_FILE))
+
+
+print(find_log())
