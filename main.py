be9ab8c2cba9dada17ac267af6ff4114b23ddf59
M_Kunysz <key999@protonmail.com>
2020-01-20 22:18:31 +0100

    Add files via upload
    a

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
-#!/usr/bin/env python3
+#!/usr/local/bin/python3
+
 # dev branch
 
 from os import popen, system
+import time
 
 # name of pattern to be searched for : amount of 'safe' occurrences
-PATTERNS = {'failed': 100,
-            'root': 100
+PATTERNS = {'failed': 20,
+            'root': 5
             }
 
-LOG_FILE = 'logs'
+LOG_FILE = 'auth.log'
 FLAG_FILE = './flag'
 LAST_LOG_LINE = 0
 FORM = None
+LAST_LOG_DATE = None
 
 
 def check():
     global FORM
     global LAST_LOG_LINE
+    global LAST_LOG_DATE
 
     # prepare searching form
     for i in PATTERNS:
@@ -31,7 +35,15 @@ def check():
     # set LAST_LOG_LINE
     LAST_LOG_LINE = popen("wc -l {0} | cut -d ' ' -f 1".format(LOG_FILE)).read()
     print('last log line:', LAST_LOG_LINE)  # shite-debug
-    save()
+
+    # set LAST_LOG_DATE
+    LAST_LOG_DATE = time.strptime(popen("tail -n 1 {0} | cut -d ' ' -f 1-3".format(LOG_FILE)).read()[0:-1],
+                                  "%b %d %H:%M:%S")
+    print('last date:', LAST_LOG_DATE)  # shite-debug
+
+    twojastara = popen("tail -n 1 {0} | cut -d ' ' -f 1-3".format(LOG_FILE)).read()
+
+    save(twojastara)
 
     # determine if being attacked
     for i in PATTERNS:
@@ -42,12 +54,14 @@ def check():
 # TODO implement notification
 def bruteforce_detected():
     system("echo 1 > {0}".format(FLAG_FILE))
+    print("RATUNKU ATAKUJO")  # shite-debug
 
 
 # save LAST_LOG_LINE
-def save():
+def save(twojstary):
     with open('last-log', 'w') as f:
         f.write(LAST_LOG_LINE)
+        f.write(twojstary)
 
 
 # load LAST_LOG_LINE
@@ -55,7 +69,7 @@ def load():
     global LAST_LOG_LINE
     try:
         with open('last-log', 'r') as f:
-            LAST_LOG_LINE = int(f.read()[:-1])
+            LAST_LOG_LINE = int(f.readline()[:-1])
     except (ValueError, FileNotFoundError):
         LAST_LOG_LINE = 0
 
